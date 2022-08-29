ec9f17ebe28707e6cc3777fce16fcf22e1efa05d
DryDish <57399961+DryDish@users.noreply.github.com>
2022-08-29 11:01:36 +0200

    format documents

--- assignment-1/python/libs/Parsers.py
+++ assignment-1/python/libs/Parsers.py
@@ -6,9 +6,11 @@ import xml
 import xmltodict
 import csv
 
+
 def parse_path(path):
     current_path = dirname(__file__)
     return current_path + "/../../files/" + path
+
 
 class Json():
     def parse(io_file: json) -> str:
@@ -19,21 +21,23 @@ class Json():
 
         return parsed_user
 
+
 class Yaml:
     def parse(io_file: yaml) -> dict:
         io_file = parse_path(io_file)
         file = open(io_file, "rt")
         parsed_user = yaml.safe_load(file.read())
-        file.close();
+        file.close()
 
-        return parsed_user;
+        return parsed_user
+
 
 class Xml:
     def parse(io_file: xml) -> dict:
         io_file = parse_path(io_file)
         file = open(io_file, "r")
         print("umm yo?")
-        parsed_user = xmltodict.parse(file.read());
+        parsed_user = xmltodict.parse(file.read())
         print("umm yu!")
         return parsed_user
 
@@ -44,11 +48,12 @@ class Csv:
         file = open(io_file, "r")
         reader = csv.DictReader(file)
 
-        return reader.__next__();
+        return reader.__next__()
+
 
 class Txt:
     def __to_dict(str1, str2) -> dict:
-        return {str1.strip() : str2.replace('\n', '')}
+        return {str1.strip(): str2.replace('\n', '')}
 
     def parse(io_file) -> dict:
         io_file = parse_path(io_file)
@@ -61,7 +66,7 @@ class Txt:
             if line.startswith('#'):
                 continue
             lines = line.split(",")
-            if (len(lines) == 2 ):
+            if (len(lines) == 2):
                 if lines[1] != '\n':
                     if (nested):
                         nested_dict.update(Txt.__to_dict(lines[0], lines[1]))
--- assignment-1/python/main.py
+++ assignment-1/python/main.py
@@ -1,11 +1,11 @@
 from libs import Json, Yaml, Xml, Csv, Txt
 from pprint import pprint
 
-json_user_string = Json.parse("user.json");
-yaml_user_string = Yaml.parse("user.yaml");
-csv_user_string = Csv.parse("user.csv");
+json_user_string = Json.parse("user.json")
+yaml_user_string = Yaml.parse("user.yaml")
+csv_user_string = Csv.parse("user.csv")
 txt_user_string = Txt.parse("user.txt")
-xml_user_string = Xml.parse("user.xml");
+xml_user_string = Xml.parse("user.xml")
 
 print("___________JSON___________")
 pprint(json_user_string, width=80)
@@ -17,4 +17,3 @@ print("___________XML____________")
 pprint(xml_user_string["root"])
 print("___________CSV____________")
 pprint(csv_user_string)
-
