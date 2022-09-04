91349c105bc3cfc159adbc4f690e2b93833ba354
DryDish <57399961+DryDish@users.noreply.github.com>
2022-09-04 15:08:10 +0200

    add python and rust solutions for part assignment-1 part 2

---
+++ assignment-2/date-servers/python/main.py
@@ -0,0 +1,26 @@
+from datetime import timezone
+from fastapi import FastAPI
+
+import uvicorn
+import datetime
+
+app = FastAPI()
+
+
+@app.get("/datetime")
+def date_time():
+    time = datetime.datetime.now(timezone.utc)
+
+    return {"utc_time_stamp:":  time.isoformat()}
+
+
+if __name__ == '__main__':
+    port = 3000
+    host = '127.0.0.1'
+    uvicorn.run(
+        "main:app",
+        port=port,
+        host=host,
+        reload=True,
+    )
+    print(f"Server running on port {port} on host {host}")
